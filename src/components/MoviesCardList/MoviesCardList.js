0ea7565582fbb75590bd4d8cd274c14610f9a48e
Valya <vshulya@gmail.com>
2022-07-21 08:11:13 +0100

    feat: add movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
+import Preloader from "../Preloader/Preloader";
 
-function MoviesCardList(props) {
+function MoviesCardList({movies, onCardClick, onCardDelete, isLoading}) {
+
+
+  //const displayMovies = moviesCards?.slice(0, initialCardsCurrent);
+
   return (
     <div className="movies">
+    {isLoading && <Preloader />}
     <ul className="movies__list">
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
+          {movies.map((movie) => (
+              <MoviesCard
+                key={movie.id}
+                movie={movie} 
+                />
+          ))}
     </ul>
     <button className="movies__button">Ещё</button>
   </div>
