4c6cd6c38e091b5180322c443838e965ee4211d3
Valya <vshulya@gmail.com>
2022-09-13 23:16:46 +0100

    change link in portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,7 +9,7 @@ function Portfolio(props) {
       <ul className="portfolio__projects">
         <li className="portfolio__project"><a href="https://vshulya.github.io/how-to-learn" className="portfolio__link link" target="_blanck" rel="noreferrer">Static website <span className="portfolio__link-arrow">&#8599;</span></a></li>
         <li className="portfolio__project"><a href="https://vshulya.github.io/russian-travel" className="portfolio__link link" target="_blanck" rel="noreferrer">Responsive website <span className="portfolio__link-arrow">&#8599;</span></a></li>
-        <li className="portfolio__project"><a href="https://mesto.nomoreparties.sbs" className="portfolio__link link" target="_blanck" rel="noreferrer">Single-page app <span className="portfolio__link-arrow">&#8599;</span></a></li>
+        <li className="portfolio__project"><a href="http://140.82.42.116/aroundtheworld" className="portfolio__link link" target="_blanck" rel="noreferrer">Single-page app <span className="portfolio__link-arrow">&#8599;</span></a></li>
       </ul>
     </section>
   );
